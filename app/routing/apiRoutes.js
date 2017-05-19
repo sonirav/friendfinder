20a60759dd90afa1357767b36c93710aaeb31613
Ravinder Soni <sonirav@gmail.com>
2017-05-18 23:28:37 -0700

    Final Push

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,13 +1,16 @@
 var friends=require("../data/friends.js");
 module.exports=function(app){
-app.get('api/friends',function(req, res){
+app.get('/api/friends',function(req, res){
 res.json(friends);
  })
-app.post('api/friends',function(req, res){var bestMatch={
-};
+
+//--------------------- Main Logic to respond back the best match
+// ------------ We user double loop to check the best match.
+app.post('/api/friends',function(req, res){
+var bestMatch={};
 var userData = req.body;
 var userScores = userData.scores;
-var totalDifference =0;
+var totalDifference = 0;
 for (var i = 0; i < userData.length; i++) {
 	totalDifference =0;
 	for (var j = 0; j < userScores.length; j++) {
@@ -16,10 +19,17 @@ for (var i = 0; i < userData.length; i++) {
 		bestMatch.name= friends[i].name;
 		bestMatch.photo= friendDifference[i].photo;
 		bestMatch.friendDifference = totalDifference;}
+		console.log(totalDifference);
 	}
 
-	friends.push()
-	res.json()
+	friends.push(req.body); // here 
+
+	
+	console.log(bestMatch);
 }
+app.get('/api/friends',function(req, res){res.json(userData);}) // Adding Data to the api
+console.log(userData);
 })
+
+
 }
